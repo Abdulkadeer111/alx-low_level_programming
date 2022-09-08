f5f819fba4eb8421b446a22a083f309361dd5072
Abdulkadeer111 <sikirullahiabdulkodir@gmail.com>
2022-09-08 16:07:35 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 
+
+
 /**
- * main - printing
  *
- * Return: Always 0 (Successful)
- * /
- int main(void)
- {
-     puts("\"Programming is like building a multilingual puzzle");
-     return (0);
-	 }
+ * * main - print the string in the put function
+ *
+ * *
+ *
+ * * Description: using the main function
+ *
+ * * this program prints "Programming is like building a multilingual puzzle
+ *
+ * * Return: 0
+ *
+ * */
+
+int main(void)
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
+}
